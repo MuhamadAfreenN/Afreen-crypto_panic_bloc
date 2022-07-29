73a10e28f9e95f75567268caaebca2872d875d40
Muhamad Afreen <muhamadafreen@techfriar.com>
2022-07-29 11:05:00 +0530

    edited in crypto services

--- lib/crypto_news/repository/crypto_services.dart
+++ lib/crypto_news/repository/crypto_services.dart
@@ -6,19 +6,6 @@ class CryptoService {
       "https://cryptopanic.com/api/v1/posts/?auth_token=249d42fc0f64643c9b088c3506ea007e17587a2b&public=true";
 
 
-
-  //
-  // Future<Crypto> getMoreNews(String pageNum) async{
-  //   final url =  "https://cryptopanic.com/api/v1/posts/?auth_token=249d42fc0f64643c9b088c3506ea007e17587a2b&public=true&page=$pageNum";
-  //   final response = await get(Uri.parse(url));
-  //   final cryptoNews = cryptoFromJson(response.body);
-  //   return cryptoNews;
-  //
-  //
-  // }
-  //  if(pageNumber!= null){
-  //  url = "https://cryptopanic.com/api/v1/posts/?auth_token=249d42fc0f64643c9b088c3506ea007e17587a2b&public=true&page=$pageNumber";
-  //  }
 /// to fetch all data from api
   Future<Crypto> getCryptoNews() async {
 
